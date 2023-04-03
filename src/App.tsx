524ca3a1a2370d993ef63cb61857d87d0cc79617
viruop <virajrai706@gmail.com>
2023-04-03 11:55:50 +0530

    description title

--- src/App.tsx
+++ src/App.tsx
@@ -12,17 +12,18 @@ function App() {
   const [currentData, setCurrentData] = useState({
     title: "",
     image: "",
+    audio: "",
   });
 
   useEffect(() => {
     const days = [
-      { title: "Sunday", image: surya },
-      { title: "Monday", image: shiva },
-      { title: "Tuesday", image: hanuman },
-      { title: "Wednesday", image: krishna },
-      { title: "Thursday", image: vishnu },
-      { title: "Friday", image: kali },
-      { title: "Sunny", image: shani },
+      { title: "Sunday", image: surya, audio: "" },
+      { title: "Monday", image: shiva, audio: "" },
+      { title: "Tuesday", image: hanuman, audio: "" },
+      { title: "Wednesday", image: krishna, audio: "" },
+      { title: "Thursday", image: vishnu, audio: "" },
+      { title: "Friday", image: kali, audio: "" },
+      { title: "Sunny", image: shani, audio: "" },
     ];
     const dayOfWeek = days[new Date().getDay()];
     setCurrentData(dayOfWeek);
@@ -34,8 +35,13 @@ function App() {
     { title: "parsad" },
     { title: "agarbatti" },
   ];
+
+  const handleClick = () => {
+    const audio = new Audio();
+    audio.play();
+  };
   return (
-    <section className="font-sans lg:max-h-screen container  m-auto flex flex-col lg:flex-row justify-center my-8">
+    <section className="font-sans h-screen  container  m-auto flex flex-col lg:flex-row justify-center ">
       <div className="order-2 lg:order-1 w-full lg:w-1/4 flex flex-col items-center lg:items-end justify-center text-center lg:text-right ml-0 lg:ml-8 mt-8">
         {leftButtons.map((i, id) => {
           return (
@@ -55,6 +61,9 @@ function App() {
       </div>
       <div className="order-1 lg:order-2 w-full lg:w-1/2 max-w-sm lg:max-w-lg md:max-w-md mx-auto mb-6 lg:mb-0">
         <img className="h-90" loading="lazy" src={currentData.image} alt="" />
+        <button className="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+          Start
+        </button>
       </div>
       <div className="order-last w-full lg:w-1/4 flex flex-col items-center lg:items-start justify-center text-center lg:text-left mt-8 mr-8">
         {leftButtons.map((i, id) => {
