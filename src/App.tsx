acd1c8ab67a3a875b8cf1d076872dfa9b82bbc9a
viraj  rai <69512213+viruop@users.noreply.github.com>
2024-09-27 15:27:47 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,7 @@ function App() {
       { title: "Friday", image: kali, audio: jaikali },
       { title: "Saturday", image: shani, audio: hanumanChalisa },
     ];
-    const dayOfWeek = days[new Date().getDay() + 0];
+    const dayOfWeek = days[new Date().getDay() - 1];
     setCurrentData(dayOfWeek);
     document.title = currentData.title;
     return () => {
